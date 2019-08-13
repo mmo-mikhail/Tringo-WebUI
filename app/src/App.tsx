8c7645707f7edd1e2503f6dc731d2e629c921f7d
dzmitry-kakaruk-webjet <dzmitry.kakaruK@webjet.com.au>
2019-08-14 01:14:56 +1000

    PBI35 testing builds

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -44,7 +44,7 @@ class App extends Component<any, any> {
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to Tringo</h1>
 					<h6 className="App-intro">
-						by the best team ever
+						by the best RMIT students ever
 					</h6>
                 </header>
                 {/*
