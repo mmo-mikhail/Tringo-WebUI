72425e18d7b74b2a970fee14f03e0c82f902e7c7
rmit-s3668804-Dzmitry-Kakaruk <s3668804@student.rmit.edu.au>
2019-08-26 03:33:20 +1000

    more map style

--- app/src/components/googleMap.tsx
+++ app/src/components/googleMap.tsx
@@ -135,65 +135,32 @@ class SimpleMap extends React.Component<MapProp, any> {
                             {
                                 featureType: 'all',
                                 elementType: 'geometry.fill',
-                                stylers: [
-                                    {
-                                        weight: '2.00'
-                                    }
-                                ]
+                                stylers: [{ weight: '2.00' }]
                             },
                             {
                                 featureType: 'all',
                                 elementType: 'geometry.stroke',
-                                stylers: [
-                                    {
-                                        color: '#9c9c9c'
-                                    }
-                                ]
+                                stylers: [{ color: '#9c9c9c' }]
                             },
                             {
                                 featureType: 'all',
                                 elementType: 'labels.text',
-                                stylers: [
-                                    {
-                                        visibility: 'on'
-                                    }
-                                ]
+                                stylers: [{ visibility: 'on' }]
+                            },
+                            {
+                                featureType: 'administrative.province',
+                                elementType: 'all',
+                                stylers: [{ visibility: 'off' }]
                             },
                             {
                                 featureType: 'landscape',
                                 elementType: 'all',
-                                stylers: [
-                                    {
-                                        color: '#f2f2f2'
-                                    }
-                                ]
-                            },
-                            {
-                                featureType: 'landscape',
-                                elementType: 'geometry.fill',
-                                stylers: [
-                                    {
-                                        color: '#ffffff'
-                                    }
-                                ]
-                            },
-                            {
-                                featureType: 'landscape.man_made',
-                                elementType: 'geometry.fill',
-                                stylers: [
-                                    {
-                                        color: '#ffffff'
-                                    }
-                                ]
+                                stylers: [{ color: '#f2f2f2' }]
                             },
                             {
                                 featureType: 'poi',
                                 elementType: 'all',
-                                stylers: [
-                                    {
-                                        visibility: 'off'
-                                    }
-                                ]
+                                stylers: [{ visibility: 'off' }]
                             },
                             {
                                 featureType: 'road',
@@ -203,41 +170,22 @@ class SimpleMap extends React.Component<MapProp, any> {
                             {
                                 featureType: 'transit',
                                 elementType: 'all',
-                                stylers: [
-                                    {
-                                        visibility: 'off'
-                                    }
-                                ]
+                                stylers: [{ visibility: 'off' }]
                             },
                             {
                                 featureType: 'water',
                                 elementType: 'all',
-                                stylers: [
-                                    {
-                                        color: '#46bcec'
-                                    },
-                                    {
-                                        visibility: 'on'
-                                    }
-                                ]
+                                stylers: [{ color: '#46BCEC' }]
                             },
                             {
                                 featureType: 'water',
                                 elementType: 'geometry.fill',
-                                stylers: [
-                                    {
-                                        color: '#c8d7d4'
-                                    }
-                                ]
+                                stylers: [{ color: '#c8d7d4' }]
                             },
                             {
                                 featureType: 'water',
                                 elementType: 'labels',
-                                stylers: [
-                                    {
-                                        visibility: 'off'
-                                    }
-                                ]
+                                stylers: [{ visibility: 'off' }]
                             }
                         ]
                     }}
