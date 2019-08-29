a3754fddedc7064dc71f35aa29df267c358bea11
rmit-s3668804-Dzmitry-Kakaruk <s3668804@student.rmit.edu.au>
2019-08-30 01:46:38 +1000

    Keep calm and commit

---
+++ app/src/reducers/rootReducer-test.tsx
@@ -0,0 +1,10 @@
+﻿import { combineReducers } from 'redux';
+import destinationsReducer from './destinationsReducer';
+
+class Interface {
+    Blah: string = 'sdasda';
+}
+
+export default combineReducers({
+    destinationsReducer
+});
