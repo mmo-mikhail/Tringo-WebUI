9323749ab62b26391c81b226170c12de2b032c01
Simon Chow <Melanmare@users.noreply.github.com>
2019-09-05 17:11:45 +1000

    finish highlight input feature

--- app/src/components/searchWidget/Autocomplete.tsx
+++ app/src/components/searchWidget/Autocomplete.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import classnames from 'classnames';
-import { Async as AsyncSelect, components } from 'react-select';
+import {Async as AsyncSelect, components} from 'react-select';
 import './styles/widget.scss';
-import { ActionMeta } from 'react-select/lib/types';
+import {ActionMeta} from 'react-select/lib/types';
 import Highlighter from 'react-highlight-words';
 
 // AsyncSelect custom components below
 const LoadingIndicator = () => <span className="loader alt" />;
 
-const Control = ({ children, ...props }: any) => {
-    const { inputIconClassName } = props.selectProps;
+const Control = ({children, ...props}: any) => {
+    const {inputIconClassName} = props.selectProps;
     const hasIcon = inputIconClassName !== '';
 
     return (
@@ -36,14 +36,23 @@ interface AutoCompleteProps {
     disabled?: boolean;
 }
 
-class Autocomplete extends React.Component<AutoCompleteProps> {
+interface AutoCompleteState {
+    inputValue?: string;
+    selectedOption: any,
+}
+
+class Autocomplete extends React.Component<AutoCompleteProps, AutoCompleteState> {
     constructor(props: AutoCompleteProps) {
         super(props);
-
         this.loadOptionsHandler = this.loadOptionsHandler.bind(this);
         this.noOptionsMessageHandler = this.noOptionsMessageHandler.bind(this);
         this.onSelectChanged = this.onSelectChanged.bind(this);
         this.formatOptionLabel = this.formatOptionLabel.bind(this);
+        this.onInputChange = this.onInputChange.bind(this);
+        this.state = {
+            inputValue: process.env.REACT_APP_DEFAULT_LOCATION,
+            selectedOption: null
+        };
     }
 
     loadOptionsHandler(inputValue: any, callback: any) {
@@ -64,45 +73,67 @@ class Autocomplete extends React.Component<AutoCompleteProps> {
         return null;
     }
 
-    onSelectChanged(value: any, action: ActionMeta) {
+    onSelectChanged(value: any, {action}: ActionMeta) {
         this.props.onChange(value);
+
+        if (action === 'clear' || value === null) {
+            this.setState({selectedOption: value, inputValue: ''}, () => {
+                console.log(`option is cleared value now is${  this.state.inputValue}`)
+            })
+            return;
+        }
+        this.setState({selectedOption: value, inputValue: value.optionLabel}, () => {
+        })
+
+
     }
 
-    formatOptionLabel({optionLabel,optionSubLabel}:any, {inputValue}:any){
+    onInputChange = (inputValue: any) => {
+        if (inputValue !== '') {
+            this.setState({inputValue: inputValue}, () => {
+                console.log(`input change triggered${  this.state.inputValue}`)
+            });
+        }
+
+    };
+
+    formatOptionLabel({optionLabel, optionSubLabel}: any, {inputValue}: any) {
+
 
         return (
-           <div>
-              <div className='format-label'>  
-                  <Highlighter
+            <div>
+                <div className='format-label'>
+                    <Highlighter
                         searchWords={[inputValue]}
                         textToHighlight={optionLabel}
                         autoEscape={true}
                         highlightClassName='high-light'
                         unhighlightClassName='un-high-light'
-                    />  
+                    />
                 </div>
-                <br/>
-                <div className='format-label'>               
-                 <Highlighter
-                    searchWords={[inputValue]}
-                    textToHighlight={optionSubLabel}
-                    autoEscape={true}
-                    highlightClassName='high-light'
-                    unhighlightClassName='un-high-light'
-                 /> 
+                <br />
+                <div className='format-label'>
+                    <Highlighter
+                        searchWords={[inputValue]}
+                        textToHighlight={optionSubLabel}
+                        autoEscape={true}
+                        highlightClassName='high-light'
+                        unhighlightClassName='un-high-light'
+                    />
                 </div>
             </div>
         );
     }
-            
-         
+
 
     render() {
+        let inputValue = this.state.inputValue;
+
         return (
             <AsyncSelect
                 inputId={this.props.id}
-                isClearable              
-                defaultInputValue={process.env.REACT_APP_DEFAULT_LOCATION}
+                isClearable
+                inputValue={this.state.inputValue}
                 placeholder={this.props.placeholder}
                 isDisabled={this.props.disabled}
                 loadOptions={this.loadOptionsHandler}
@@ -110,8 +141,9 @@ class Autocomplete extends React.Component<AutoCompleteProps> {
                 formatOptionLabel={this.formatOptionLabel}
                 className={classnames('wj-rc-autocomplete', this.props.className)}
                 classNamePrefix="rc-autocomplete"
-                components={{ Control, LoadingIndicator, Input }}
+                components={{Control, LoadingIndicator}}
                 onChange={this.onSelectChanged}
+                onInputChange={this.onInputChange}
             />
         );
     }
