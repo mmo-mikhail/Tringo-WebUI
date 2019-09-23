74221200770efa28cfd200a5882b9975df586525
Simon Chow <Melanmare@users.noreply.github.com>
2019-09-23 17:36:56 +1000

    format code

--- app/src/components/markers/departureMarker.tsx
+++ app/src/components/markers/departureMarker.tsx
@@ -4,8 +4,8 @@ import {GoogleMapRequiredProps} from './priceTagMarker';
 
 const DepartureMarker: React.FC<GoogleMapRequiredProps> = () => {
     return <img alt="" className="departure-marker "
-    src="https://services.webjet.com.au/web/hotels/search/content/images/google-map-pin.png" />;
-    
+                src="https://services.webjet.com.au/web/hotels/search/content/images/google-map-pin.png"/>;
+
 };
 
 export default DepartureMarker;
