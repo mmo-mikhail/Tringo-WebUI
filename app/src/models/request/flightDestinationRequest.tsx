3787b83cd3266385257db0373c64c6454f9a1302
Michael Muzyka <MMuzyka@verismic.com>
2019-10-11 03:04:14 +1100

    add caching after getting all flights

--- app/src/models/request/flightDestinationRequest.tsx
+++ app/src/models/request/flightDestinationRequest.tsx
@@ -1,12 +1,25 @@
 import { DatesInput } from './dateInput';
 
-export class FlightDestinationRequest {
+export class BaseFlightDestinationRequest {
+    constructor(
+        public departureAirportId: string, // as IATA
+        public dates: DatesInput
+    ) {}
+
+    static createRandom(): FlightDestinationRequest {
+        return new FlightDestinationRequest('MEL', MapArea.createRandom(), new Budget(0, 1000), new DatesInput(-1));
+    }
+}
+
+export class FlightDestinationRequest extends BaseFlightDestinationRequest {
     constructor(
         public departureAirportId: string, // as IATA
         public searchArea: MapArea,
         public budget: Budget | null, // null only when budget is 'any'
         public dates: DatesInput
-    ) {}
+    ) {
+        super(departureAirportId, dates);
+    }
 
     static createRandom(): FlightDestinationRequest {
         return new FlightDestinationRequest('MEL', MapArea.createRandom(), new Budget(0, 1000), new DatesInput(-1));
--- app/src/sagas/rootSaga.tsx
+++ app/src/sagas/rootSaga.tsx
@@ -1,6 +1,6 @@
 import { all, takeLatest } from 'redux-saga/effects';
 import { destinationActionType } from '../actions/actionTypes';
-import * as destinationSaga from './destinationSaga';
+import * as destinationSaga from './cachedDestinationSaga';
 
 export function* rootSaga() {
     yield all([takeLatest(destinationActionType.FETCH_DESTINATION_START, destinationSaga.fetchDestinationsSaga)]);
